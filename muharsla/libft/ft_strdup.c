2317a871d87e19f65654dff3e36432d6c29cf710
azizcan1212 <tamazizcan407@gmail.com>
2025-07-28 14:50:00 +0300

    Expand tamamlandı executa göre hata mesajları için  veya hata çıkması durumunda geri dönülecek . Ayrıca kontrol için debug eklendi.Proje sonunda onlar silinecek

---
+++ muharsla/libft/ft_strdup.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: atam <atam@student.42kocaeli.com.tr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/20 17:23:34 by atam              #+#    #+#             */
+/*   Updated: 2024/11/06 15:28:19 by atam             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdlib.h>
+
+char	*ft_strdup(const char *s1)
+{
+	char	*str;
+	int		len;
+
+	len = ft_strlen(s1);
+	str = (char *)malloc(len +1);
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	ft_strlcpy(str, s1, len + 1);
+	return (str);
+}
---
+++ muharsla/utils/utils.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: muharsla <muharsla@student.42kocaeli.co    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/11 16:44:16 by muharsla          #+#    #+#             */
+/*   Updated: 2025/07/11 16:44:18 by muharsla         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include "../libft/libft.h"
+
+char *ft_strndup(const char *s, unsigned int n)
+{
+    unsigned int len;
+    char *dup;
+
+    if (!s)
+        return NULL;
+
+    len = ft_strlen(s);
+    if (len > n)
+        len = n;
+
+    dup = (char *)malloc(len + 1);
+    if (!dup)
+        return NULL;
+
+    ft_strlcpy(dup, s, len + 1);
+    return dup;
+}
+
+int ft_isspace(char c)
+{
+    return (
+        c == ' '  ||  // space
+        c == '\t' ||  // tab
+        c == '\n' ||  // newline
+        c == '\v' ||  // vertical tab
+        c == '\f' ||  // form feed
+        c == '\r' ||    // carriage return
+        c == '\\' ||  // backslash
+        c == ';' // semicolon
+
+    );
+}
