3ad69578f030d119bb78587be8152e9caa4575c7
azizcan1212 <tamazizcan407@gmail.com>
2025-09-11 14:46:54 +0300

    child sinyal ve açık fd sorunu  çözüldü

---
+++ minishellx/minishell-muhammed/shell/parse/parse_to_command.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse_to_command.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: muharsla <muharsla@student.42kocaeli.co    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/11 16:43:54 by muharsla          #+#    #+#             */
+/*   Updated: 2025/08/07 15:45:57 by muharsla         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+t_command	*parse_tokens_to_commands(t_token *tokens)
+{
+	t_command	*head;
+	t_command	*cur;
+	int			argc;
+
+	head = NULL;
+	cur = NULL;
+	argc = 0;
+	while (tokens)
+		process_one_token(&tokens, &cur, &head, &argc);
+	if (cur)
+		cur->args[argc] = NULL;
+	return (head);
+}
